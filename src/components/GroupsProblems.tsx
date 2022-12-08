71e364691f788f4291ece1956f814323d53db28f
LucasRGuimaraes <lukasmarinho2008@gmail.com>
2022-12-08 11:25:33 -0300

    Ajustes GroupsProblems

--- src/components/GroupsProblems.tsx
+++ src/components/GroupsProblems.tsx
@@ -5,7 +5,6 @@ import Box from "@mui/material/Box";
 import { DataGrid, GridColDef, GridValueGetterParams } from "@mui/x-data-grid";
 
 const columns: GridColDef[] = [
-  { field: "id", headerName: "ID", width: 90 },
   {
     field: "name",
     headerName: "Name",
@@ -28,11 +27,11 @@ const columns: GridColDef[] = [
 ];
 
 const rows = [
-  { id: 1, name: "Storage", severity: "Severe", hostsQuantity: 1 },
-  { id: 2, name: "Prometheus", severity: "Medium", hostsQuantity: 3 },
-  { id: 3, name: "Routers", severity: "Server", hostsQuantity: 1 },
-  { id: 4, name: "Backup", severity: "Low", hostsQuantity: 2 },
-  { id: 5, name: "DNS", severity: "Medium", hostsQuantity: 4 },
+  { name: "Storage", severity: "Severe", hostsQuantity: 1 },
+  { name: "Prometheus", severity: "Medium", hostsQuantity: 3 },
+  { name: "Routers", severity: "Server", hostsQuantity: 1 },
+  { name: "Backup", severity: "Low", hostsQuantity: 2 },
+  { name: "DNS", severity: "Medium", hostsQuantity: 4 },
 ];
 
 export function GroupsProblems() {
@@ -41,14 +40,14 @@ export function GroupsProblems() {
       <CardHeader title="Groups" />
       <Divider />
       <CardContent>
-        <Box sx={{ height: "100%", width: "100%"}}>
+        <Box sx={{ height: "100%", width: "100%" }}>
           <DataGrid
             rows={rows}
             columns={columns}
-            pageSize={4}
-            rowsPerPageOptions={[5]}
+            pageSize={5}
             disableSelectionOnClick
             experimentalFeatures={{ newEditingApi: true }}
+            getRowId={() => Math.random()}
           />
         </Box>
       </CardContent>
